6f0144293ae5835ca38312966cce8e3941ecffd2
stmmmm <kos.stamatopoulos@gmail.com>
2021-10-27 15:53:13 +0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# angular-ivy-r8enb7
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/angular-ivy-r8enb7)
---
+++ src/project/hello.component.ts
@@ -0,0 +1,10 @@
+import { Component, Input } from '@angular/core';
+
+@Component({
+  selector: '',
+  template: ``,
+  styles: [``]
+})
+export class HelloComponent  {
+  @Input() name: string;
+}
